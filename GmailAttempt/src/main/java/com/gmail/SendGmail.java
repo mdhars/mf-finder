d562be8c40a61e5eb22b8aaee37339d3fe01745f
Muralidharan Sukumaran <musukumaran@paypal.com>
2016-02-20 01:06:19 +0530

    first webapp

--- GmailAttempt/src/main/java/com/gmail/SendGmail.java
+++ GmailAttempt/src/main/java/com/gmail/SendGmail.java
@@ -47,4 +47,42 @@ public class SendGmail {
 			throw new RuntimeException(e);
 		}
 	}
+	
+	public static void send(){
+		 {
+				
+				Properties props = new Properties();
+				props.put("mail.smtp.host", "smtp.gmail.com");
+				props.setProperty("mail.smtp.ssl.enable", "true");
+				props.put("mail.smtp.socketFactory.port", "465");
+				props.put("mail.smtp.socketFactory.class","javax.net.ssl.SSLSocketFactory");
+				props.put("mail.smtp.auth", "true");
+				props.put("mail.smtp.port", "465");
+
+				Session session = Session.getInstance(props);/*,
+					new javax.mail.Authenticator() {
+						protected PasswordAuthentication getPasswordAuthentication() {
+							return new PasswordAuthentication("muralisuk@gmail.com","chaseg0als");
+						}
+					});*/
+
+				try {
+
+					Message message = new MimeMessage(session);
+					message.setFrom(new InternetAddress("muralisuk@gmail.com"));
+					message.setRecipients(Message.RecipientType.TO,
+							InternetAddress.parse("mdhars@gmail.com"));
+					message.setSubject("Testing Subject");
+					message.setText("Dear Mail Crawler," +
+							"\n\n No spam to my email, please!");
+
+					Transport.send(message,"muralisuk@gmail.com","chaseg0als");
+
+					System.out.println("Done");
+
+				} catch (MessagingException e) {
+					throw new RuntimeException(e);
+				}
+			}
+	}
 }
